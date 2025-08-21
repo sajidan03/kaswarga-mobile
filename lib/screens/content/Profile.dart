7a0d64ac228c2e3265cadeb765dfa2c5f725f53d
si-eja <fauzan57889@gmail.com>
2025-08-21 15:11:23 +0700

    sajidna

--- lib/screens/content/Profile.dart
+++ lib/screens/content/Profile.dart
@@ -10,11 +10,11 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(36),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // supaya ke kiri
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
               'Profiles',
@@ -25,10 +25,41 @@ class _ProfileState extends State<Profile> {
               ),
               textAlign: TextAlign.left,
             ),
-            const SizedBox(height: 20), // kasih jarak
-            const CircleAvatar(
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                CircleAvatar(
               radius: 60,
               backgroundImage: AssetImage('assets/akaza.jpg'),
+            ),
+            SizedBox(width: 10,),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Column(
+                children: [
+                     Text(
+              'Hakuji Akaza',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.left,
+            ),
+               Text(
+              'Hakuji Akaza',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
+              textAlign: TextAlign.left,
+            ),
+                ],
+              ),
+            )
+            
+              ],
             ),
           ],
         ),
